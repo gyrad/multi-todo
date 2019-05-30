19bac391502acc8d8ce4e99774c79100bbb77750
Gaychey <tgaychey@gmail.com>
2019-05-29 22:41:39 -0400

    Updates

---
+++ precache-manifest.c668415f73cb76594622fe16ed72f53e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49d54bb64f18f36a37b528a6c414a46b",
+    "url": "/multi-todo/index.html"
+  },
+  {
+    "revision": "bf30445ac71dc42a0554",
+    "url": "/multi-todo/static/css/2.62908225.chunk.css"
+  },
+  {
+    "revision": "896f383d0a96c03bc411",
+    "url": "/multi-todo/static/css/main.dea700f9.chunk.css"
+  },
+  {
+    "revision": "bf30445ac71dc42a0554",
+    "url": "/multi-todo/static/js/2.54c894eb.chunk.js"
+  },
+  {
+    "revision": "896f383d0a96c03bc411",
+    "url": "/multi-todo/static/js/main.6877a6bf.chunk.js"
+  },
+  {
+    "revision": "6f0e4a6decc289f247b3",
+    "url": "/multi-todo/static/js/runtime~main.d37eec7a.js"
+  }
+]);
