f165465b9c0bec3ce6d3c9c29166166b400a9135
Gaychey <tgaychey@gmail.com>
2019-08-05 16:28:33 -0400

    Updates

---
+++ precache-manifest.b6c539e482838e6404ca54d3fb20010e.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a35d5d2afd96eaba5af75cf13b5a8f4f",
+    "url": "/multi-todo/index.html"
+  },
+  {
+    "revision": "1f4d943f8d2958dba0c1",
+    "url": "/multi-todo/static/css/2.7616ceee.chunk.css"
+  },
+  {
+    "revision": "658cfcab7b45fdc2046a",
+    "url": "/multi-todo/static/css/main.a61f0005.chunk.css"
+  },
+  {
+    "revision": "1f4d943f8d2958dba0c1",
+    "url": "/multi-todo/static/js/2.d593b7d4.chunk.js"
+  },
+  {
+    "revision": "658cfcab7b45fdc2046a",
+    "url": "/multi-todo/static/js/main.06361790.chunk.js"
+  },
+  {
+    "revision": "6f0e4a6decc289f247b3",
+    "url": "/multi-todo/static/js/runtime~main.d37eec7a.js"
+  },
+  {
+    "revision": "3165b14bbf3b64fca65829ecde6b800d",
+    "url": "/multi-todo/static/media/fa-brands-400.3165b14b.eot"
+  },
+  {
+    "revision": "457cb96b6191ed105c4fe8463957f70d",
+    "url": "/multi-todo/static/media/fa-brands-400.457cb96b.woff"
+  },
+  {
+    "revision": "f249e44df3044e7b0d665b550569ddf0",
+    "url": "/multi-todo/static/media/fa-brands-400.f249e44d.svg"
+  },
+  {
+    "revision": "f2e186cfab4787d4ef6f1bb192aa9a1b",
+    "url": "/multi-todo/static/media/fa-brands-400.f2e186cf.ttf"
+  },
+  {
+    "revision": "f861a57c52ef711cf807a3eec92c0e17",
+    "url": "/multi-todo/static/media/fa-brands-400.f861a57c.woff2"
+  },
+  {
+    "revision": "2cd8d991e82712f1a5c5de8ee869ca74",
+    "url": "/multi-todo/static/media/fa-regular-400.2cd8d991.ttf"
+  },
+  {
+    "revision": "2fad4ee0a47f41bc76e21c2baf613182",
+    "url": "/multi-todo/static/media/fa-regular-400.2fad4ee0.svg"
+  },
+  {
+    "revision": "7ab2cb050690f93994c46258d49af5b6",
+    "url": "/multi-todo/static/media/fa-regular-400.7ab2cb05.woff"
+  },
+  {
+    "revision": "a03df7ab0ffc96cabbfe3dc71c60baaa",
+    "url": "/multi-todo/static/media/fa-regular-400.a03df7ab.eot"
+  },
+  {
+    "revision": "bd52a727b5449dc3f8195b72c9c58341",
+    "url": "/multi-todo/static/media/fa-regular-400.bd52a727.woff2"
+  },
+  {
+    "revision": "2cd2be177470d5096992572176bbe76e",
+    "url": "/multi-todo/static/media/fa-solid-900.2cd2be17.woff2"
+  },
+  {
+    "revision": "5ee32f5c8598e1a63ddf1aad4ffe54f4",
+    "url": "/multi-todo/static/media/fa-solid-900.5ee32f5c.ttf"
+  },
+  {
+    "revision": "657a469469a2fc38a2901c68a3d56512",
+    "url": "/multi-todo/static/media/fa-solid-900.657a4694.woff"
+  },
+  {
+    "revision": "a547e21eceadf53602caf057be9ad9fd",
+    "url": "/multi-todo/static/media/fa-solid-900.a547e21e.eot"
+  },
+  {
+    "revision": "fdc155d57b7351ef85b3028ea3cfc048",
+    "url": "/multi-todo/static/media/fa-solid-900.fdc155d5.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/multi-todo/precache-manifest.4e219c1aeda35dd7ba3600d83bf30eb0.js"
+  "/multi-todo/precache-manifest.b6c539e482838e6404ca54d3fb20010e.js"
 );
 
 self.addEventListener('message', (event) => {
